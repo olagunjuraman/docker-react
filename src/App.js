346660f4954d856ec07673d9a42f6bcef6c3146b
raman <olagunjuraman@gmail.com>
2020-11-08 05:05:30 +0100

    work

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to lie
+          Edit <code>src/App.js</code> and save london
         </p>
         <a
           className="App-link"
